e0e272ebffc574c5b5064a318bb5ba1b57b3247e
xmadking <ahmad.kh.se@outlook.com>
2023-06-25 03:49:05 +0200

    updated a few things

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='lol-esports-api',
-    version='0.1',
+    name='lol-esp-api',
+    version='0.23',
     license='MIT',
     author="xMadKing",
     author_email='ahmad.kh.se@outlook.com',
-    packages=find_packages('lol_esports_api'),
-    package_dir={'': 'lol_esports_api'},
+    packages=find_packages('lol_esp_api'),
+    package_dir={'': 'lol_esp_api'},
     url='https://github.com/xMadKing/lol-esports-api',
     keywords='League of legends, esports, lolesports, API',
     install_requires=[

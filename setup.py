1cf7cc9bef8e2a0ab8c1db810bb250a9ec86342d
xmadking <ahmad.kh.se@outlook.com>
2023-07-05 07:17:42 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lol-esp-api',
-    version='0.24.5',
+    version='0.3',
     license='MIT',
     author="xMadKing",
     author_email='ahmad.kh.se@outlook.com',
